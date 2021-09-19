168ecb55c47c8f1f4db701d3181007104ba3e653
fardin2689 <fakurian.fardin@gmail.com>
2021-09-19 20:43:21 +0430

    Add schema

---
+++ server/src/schema.js
@@ -0,0 +1,54 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+    token: String
+  }
+
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): User
+  }
+`;
+
+module.exports = typeDefs;
